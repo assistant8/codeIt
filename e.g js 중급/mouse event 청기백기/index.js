3307137fc2a0c945cffb302682af2e1e17526244
JoSooha <dlrhdcjs@naver.com>
2022-12-30 23:42:45 +0900

    1230 eg 답 반영

--- e.g js 중급/mouse event 청기백기/index.js
+++ e.g js 중급/mouse event 청기백기/index.js
@@ -5,10 +5,15 @@ function reset() {
   document.querySelector('.up').classList.remove('up');
 }
 
-// 1. flagUp 함수를 완성해 주세요
 function flagUp(e) {
-  // 여기에 코드를 작성해 주세요
-
+  if(e.button==0){
+    flagBlue.classList.toggle('up')
+    //flagBlue.classList.add('up') 가능
+    //이것의 의미는 flagblue 클래스 가진 애들에게 up이라는 클래스 하나 더 주는 것임 
+  }
+  else if(e.button==2) {
+    flagWhite.classList.toggle('up')
+  }
 
   // 500 밀리초 뒤에 reset함수를 실행
   setTimeout(reset, 500);
@@ -16,8 +21,7 @@ function flagUp(e) {
 
 // 2. 마우스 오른쪽 버튼 클릭시 나타나는 메뉴창을 막아주세요 
 document.addEventListener('contextmenu', function (event) {
-  // 여기에 코드를 작성하세요
-
+  event.preventDefault()
 });
 
 // 테스트 코드
--- e.g js 중급/키보드 이벤트 채팅앱/index.js
+++ e.g js 중급/키보드 이벤트 채팅앱/index.js
@@ -19,3 +19,14 @@ function sendMyText() {
 send.addEventListener('click', sendMyText);
 
 // 여기에 코드를 작성하세요
+function sendMyText2(e){
+  if(e.key === 'Enter' && !e.shiftKey){
+    //이벤트가 발생할 때 shift키를 눌렀는지를 불린 형태로 담고 있어요.
+    sendMyText();
+    e.preventDefault();
+    //원래 textarea 태그에 커서를 두고 enter키 버튼을 누르면 줄바꿈이 됩니다. 이건 브라우저의 기본 동작
+    
+  }
+  
+}
+input.addEventListener('keypress', sendMyText2)
