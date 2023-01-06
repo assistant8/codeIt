84ee17026c012812167957ca8d71613b0020d964
JoSooha <dlrhdcjs@naver.com>
2023-01-06 18:54:59 +0900

    eg

--- e.g js 중급/4535/index.js
+++ e.g js 중급/4535/index.js
@@ -2,7 +2,7 @@ const nameInput = document.querySelector('#user-name');
 const phoneInput = document.querySelector('#phone-number');
 const findBtn = document.querySelector('#find');
 
-const data = [
+const data = [ //전역변수로 선언한 듯
   { userName: '막대기', phoneNumber: '01012341111', email: 'stick@go_do_it.kr' },
   { userName: 'young', phoneNumber: '01012342222', email: 'kang@go_do_it.kr' },
   { userName: '코린이', phoneNumber: '01012343333', email: 'corin2@go_do_it.kr' },
@@ -16,11 +16,13 @@ const data = [
 ];
 
 function findEmail() {
-  const nameValue = nameInput.value;
+  const nameValue = nameInput.value; //html에서의 input 위치 가져와서 그 인풋의 값 가져옴
   const phoneValue = phoneInput.value;
 
   // 여기에 코드를 작성하세요
-  const user = 
+  const user = data.find((info)=>{ //배열에서 각 요소마다 접근
+    return (info.userName===nameValue && info.phoneNumber===phoneValue) //이러한 조건인 요소 객체 통째로 반환 => user
+  })
 
 
   const message = user
@@ -30,4 +32,4 @@ function findEmail() {
   alert(message);
 }
 
-findBtn.addEventListener('click', findEmail);
+findBtn.addEventListener('click', findEmail); //find 클릭 누르면 위 함수 발동
