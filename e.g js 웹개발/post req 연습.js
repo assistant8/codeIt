3a2ce90d77ab8d173606ea5d756578edb0c26363
JoSooha <dlrhdcjs@naver.com>
2023-01-08 20:22:19 +0900

    eg post get 실습

--- e.g js 웹개발/post req 연습.js
+++ e.g js 웹개발/post req 연습.js
@@ -1,6 +1,6 @@
-fetch("https://learn.codeit.kr/api/members", {
-    method: 'POST',
-    body: JSON.stringify(newMember)
+fetch("https://learn.codeit.kr/api/members", { //두번째 파라미터는 객체 형식
+    method: 'POST', //메소드 특정
+    body: JSON.stringify(newMember) //json 형태로 추가해줘야 하므로 string화
 })
     .then((response) => (response.text()))
     .then((result) => (console.log(result)));
