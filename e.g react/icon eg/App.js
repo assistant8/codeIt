276885b3ee35dfe2594395c1357f75fb406e32b6
JoSooha <dlrhdcjs@naver.com>
2023-01-18 14:05:57 +0900

    eg

---
+++ e.g react/icon eg/App.js
@@ -0,0 +1,11 @@
+import Hand from "./HandIcon.js"
+
+function App() {
+  return (
+    <div>
+    <Hand />
+    </div>
+    )
+}
+
+export default App;
---
+++ e.g react/icon eg/HandIcon.js
@@ -0,0 +1,12 @@
+// 여기에 코드를 작성하세요
+import rock from "./assets/rock.svg"
+
+function HandIcon() {
+  return (
+    <div>
+      <img src={rock} alt="rock"/> //이미지 안보이면 alt 보임
+    </div>
+    )
+}
+
+export default HandIcon
