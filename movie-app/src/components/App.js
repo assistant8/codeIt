db5218dc82bc77e6a9959ec63294755e36a3a2ef
JoSooha <dlrhdcjs@naver.com>
2023-01-28 01:10:13 +0900

    eg 영화 삭제 버튼

--- movie-app/src/components/App.js
+++ movie-app/src/components/App.js
@@ -1,14 +1,12 @@
 import ReviewList from "./ReviewList";
-import items from "../mock.json"
+import mockItems from "../mock.json"
 import { useState } from "react";
 
 function App({}) {
-    //리뷰리스트.js가 아닌 app에서 하는건 버튼으로 조절 가능하고, 다른 순서로도 정렬 컨트롤 하게 되도록
     const [sort, setSort] = useState("id")
+    const [items, setItems] = useState(mockItems) //배열 수정도 useState로
     const sortedItem = items.sort((a,b) => b[sort]-a[sort]) 
-    
-    //각 함수 내에서 sort 함수 쓰니까 안됨 ;;
-    //하나의 sortedItem 함수를 공용으로 쓰고 id, rating으로 할지를 정해줌
+
     const handleRatingSort = () => {
         setSort("rating")
     }
@@ -17,11 +15,19 @@ function App({}) {
         setSort("id")
     }
 
+    const handleDelete = (id) => {
+        console.log(11)
+        const newItems = items.filter((item)=>{
+            return item.id !== id
+        })
+        setItems(newItems)
+    }
+
     return (
         <div>
             <button onClick={handleRatingSort}>rating</button>
             <button onClick={handleIdSort}>id</button>
-            <ReviewList items={sortedItem} />  //
+            <ReviewList items={sortedItem} onDelete={handleDelete}/>  
         </div>
     );
 }
--- movie-app/src/components/ReviewList.js
+++ movie-app/src/components/ReviewList.js
@@ -1,6 +1,7 @@
 import "./ReviewList.css"
 
-function ReviewListItem ({item}) {
+function ReviewListItem ({item, onDelete}) {
+    const handleDeleteClick = () => onDelete(item.id)
     return (
         <div>
             <img className="img1" src={item.imgUrl}></img>
@@ -8,18 +9,19 @@ function ReviewListItem ({item}) {
                 <p>id : {item.id}</p>
                 <p>rating : {item.rating}</p>
                 <p>{item.content}</p>
+                <button onClick={handleDeleteClick}>삭제</button>
             </div>
             <hr></hr>
         </div>
     )
 }
 
-function ReviewList ({items}) {
+function ReviewList ({items, onDelete}) {
     return (
         <div>
             <ul>
                 {items.map((item, index) => {
-                    return <li key={index}>{<ReviewListItem item={item} />} </li>
+                    return <li key={index}>{<ReviewListItem item={item} onDelete={onDelete}/>} </li>
                 })}
             </ul>
         </div>
