c678928336227fcea4111d4352f7708eb5810340
JoSooha <dlrhdcjs@naver.com>
2023-01-27 15:58:34 +0900

    eg 푸드잇 정렬

--- foodit/src/components/App.js
+++ foodit/src/components/App.js
@@ -1,10 +1,21 @@
 import FoodList from './FoodList';
 import items from '../mock.json';
+import {useState} from "react"
 
 function App() {
+  const [sort, setSort] = useState("createdAt")
+  const sortedItem = items.sort((a,b) => b[sort]-a[sort])
+  const handleCreatedSort = () => {
+    setSort("createdAt")
+  }
+  const handleCalorieSort = () => {
+    setSort("calorie")
+  }
   return (
     <div>
-      <FoodList items={items} />
+      <button onClick={handleCreatedSort}>최신순</button>
+      <button onClick={handleCalorieSort}>칼로리순</button>
+      <FoodList items={sortedItem} />
     </div>
   );
 }
