2423c95d69f81058c41add7b4eabc594c773dad3
JoSooha <dlrhdcjs@naver.com>
2023-01-27 13:37:50 +0900

    eg 리뷰리스트 부가정보 추가

--- movie-app/src/components/ReviewList.js
+++ movie-app/src/components/ReviewList.js
@@ -1,9 +1,24 @@
+import "./ReviewList.css"
+
+function ReviewListItem ({item}) {
+    return (
+        <div>
+            <img className="img1" src={item.imgUrl}></img>
+            <div>
+                <p>{item.id}</p>
+                <p>{item.rating}</p>
+                <p>{item.content}</p>
+            </div>
+        </div>
+    )
+}
+
 function ReviewList ({items}) {
     return (
         <div>
             <ul>
                 {items.map((item) => {
-                    return <li>{item.title}</li>
+                    return <li>{<ReviewListItem item={item} />} </li>
                 })}
             </ul>
         </div>
