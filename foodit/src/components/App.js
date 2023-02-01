318cc03f523a1dc0036072aef77bc884d22347ba
JoSooha <dlrhdcjs@naver.com>
2023-02-02 02:12:30 +0900

    eg 푸드잇 로딩시 버튼 비활, 에러 시 에러메시지 표출

--- foodit/src/components/App.js
+++ foodit/src/components/App.js
@@ -4,9 +4,10 @@ import FoodList from './FoodList';
 
 function App() {
   const [order, setOrder] = useState('createdAt');
+  const [cursor, setCursor] = useState(null);
   const [items, setItems] = useState([]);
-  const [cursor, setCursor] = useState(null); // 커서 처음
-  const [isFinished, setIsFinished] = useState(false) //끝났을때 true로 할 것 
+  const [isLoading, setIsLoading] = useState(false) //
+  const [loadingError, setLoadingError] = useState("") //
 
   const handleNewestClick = () => setOrder('createdAt');
 
@@ -17,30 +18,41 @@ function App() {
     setItems(nextItems);
   };
 
-  // nextCursor가 없어도 마지막 페이지는 render 해야지(주의), 그래서 현재 cursor 기준으로 함
-  // cursor 없으면 (처음임) 전체 렌더
-  // cursor 있으면 (더보기겠지) 이전 데이터 + cursor 이용 새 데이터
-  // 쿼리 보낸거 받아오고 그걸 cursor로 할당 
-  const handleLoad = async (orderQuery) => {
-    const { foods, paging } = await getFoods(orderQuery);
-    if(!orderQuery.cursor) { //처음 렌더링 시 - 커서 없을테니
-      setItems(foods);
-    } else { //더보기 눌렀을 때
-      setItems((prevItems)=>[...prevItems, ...foods])
+  const handleLoad = async (options) => {
+    let result; //이렇게 선언해서 나눈건 try문 블록 안에서 선언하면 아래서 사용 불가 undef
+    try {
+      setLoadingError(null)
+      setIsLoading(true) //이게 위에 있어야 먼저 버튼이 비활되고 불러오기 시작하지
+      result = await getFoods(options); 
+    } catch (e) {
+      setLoadingError(e) //e를 객체로 받아 아래에서 띄워주려고
+      return
+    } finally {
+      setIsLoading(false) //catch에 return이 있어도 그 직전에 finally와서 코드 실행하고 return으로 돌아감
     }
-    setCursor(paging.nextCursor);
+    //이 이후는 실행 finally 이후 (에러 시 catch에서 return만 하지 않는 이상)
+    const { foods, paging: { nextCursor } } = result;
+    if (!options.cursor) {
+      setItems(foods);
+    } else {
+      setItems((prevItems) => [...prevItems, ...foods]);
+    }
+    setCursor(nextCursor);
   };
-  
-  // 더보기 버튼 누를 때
+
   const handleLoadMore = () => {
-    handleLoad({order, cursor}); //더 받을 땐 cursor 이용 / 받는 함수 파라미터가 orderQuery (객체 이므로)
+    handleLoad({
+      order,
+      cursor,
+    });
   };
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
-  //처음과 order 바꿀 때
   useEffect(() => {
-    handleLoad({order}); //첨엔 cursor 필요 없지
+    handleLoad({
+      order,
+    });
   }, [order]);
 
   return (
@@ -48,9 +60,11 @@ function App() {
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCalorieClick}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete} />
-      <button disabled={!cursor} onClick={handleLoadMore}>더보기</button>
+      {cursor && <button disabled={isLoading} onClick={handleLoadMore}>더보기</button>}
+      {loadingError?.message && <span>{loadingError.message}</span> }
     </div>
-  ); //현 cursor가 없으면 (nextcursor로 할당받을 때 null) 더보기 금지
+  ); //e 객체 안에 있는 message 프로퍼티
+  //loadingError가 있는지 먼저 확인 후 그리고 message가 있는지 2번 확인하는 느낌?
 }
 
 export default App;
