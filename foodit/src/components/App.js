7543de57309d0e0b9f4e88c66275eb7680419689
JoSooha <dlrhdcjs@naver.com>
2023-02-02 00:46:04 +0900

    eg cursor 페이지네이션 주석 추가

--- foodit/src/components/App.js
+++ foodit/src/components/App.js
@@ -17,8 +17,10 @@ function App() {
     setItems(nextItems);
   };
 
-  // nextCursor가 있으면 쿼리 보낸거 받아오고 그걸 cursor로 할당 
-  // null 이면 더보기 버튼 못누르게
+  // nextCursor가 없어도 마지막 페이지는 render 해야지(주의), 그래서 현재 cursor 기준으로 함
+  // cursor 없으면 (처음임) 전체 렌더
+  // cursor 있으면 (더보기겠지) 이전 데이터 + cursor 이용 새 데이터
+  // 쿼리 보낸거 받아오고 그걸 cursor로 할당 
   const handleLoad = async (orderQuery) => {
     const { foods, paging } = await getFoods(orderQuery);
     if(!orderQuery.cursor) { //처음 렌더링 시 - 커서 없을테니
@@ -31,14 +33,14 @@ function App() {
   
   // 더보기 버튼 누를 때
   const handleLoadMore = () => {
-    handleLoad({order, cursor}); //받는 함수 파라미터가 orderQuery (객체 이므로)
+    handleLoad({order, cursor}); //더 받을 땐 cursor 이용 / 받는 함수 파라미터가 orderQuery (객체 이므로)
   };
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   //처음과 order 바꿀 때
   useEffect(() => {
-    handleLoad({order});
+    handleLoad({order}); //첨엔 cursor 필요 없지
   }, [order]);
 
   return (
@@ -48,7 +50,7 @@ function App() {
       <FoodList items={sortedItems} onDelete={handleDelete} />
       <button disabled={!cursor} onClick={handleLoadMore}>더보기</button>
     </div>
-  );
+  ); //현 cursor가 없으면 (nextcursor로 할당받을 때 null) 더보기 금지
 }
 
 export default App;
