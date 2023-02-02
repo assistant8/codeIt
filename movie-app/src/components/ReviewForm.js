c4a3656c2bcdaafa67b1edaae4af23753e654abf
JoSooha <dlrhdcjs@naver.com>
2023-02-02 18:22:20 +0900

    eg 하나의 state로 묶음

--- movie-app/src/components/ReviewForm.js
+++ movie-app/src/components/ReviewForm.js
@@ -3,30 +3,30 @@ import "./ReviewForm.css"
 
 function ReviewForm() {
 
-    const [title, setTitle] = useState("")
-    const [rating, setRating] = useState(0);
-    const [content, setContent] = useState('');
+    const [values, setValues] = useState({
+        title : "",
+        rating : 0,
+        content : ""
+    })
 
-
-    const handleTitleChange = (e) => { //변할때마다 title로 state 변경
-        setTitle(e.target.value)
+    const handleChange = (e) => {
+        const {name, value} = e.target; //target은 input 태그겠고, 그 안에 프로퍼티 name, value만 갖고 오겠다
+        //위는 방금 변경된 데이터
+        setValues({...values, [name]: value}) //변경 안된건 values에, 방금 이벤트 된 value를 추가
     }
 
-    const handleRatingChange = (e) => {
-        const nextRating = Number(e.target.value);
-        setRating(nextRating);
-    };
-    
-    const handleContentChange = (e) => {
-        setContent(e.target.value);
-    };
+    const handleSubmit = (e) => { 
+        e.preventDefault() // 원래 바로 전송되는 기본 성질 (페이지 초기화) 막기 위함
+        console.log(values)
+    }
 
     return (
-        <form className="ReviewForm">
-          <input value={title} onChange={handleTitleChange} />
-          <input type="number" value={rating} onChange={handleRatingChange} />
-          <textarea value={content} onChange={handleContentChange} />
-        </form>
+        <form className="ReviewForm" onSubmit={handleSubmit}> 
+          <input name="title" value={values.title} onChange={handleChange} /> 
+          <input type="number" name="rating" value={values.rating} onChange={handleChange} />
+          <textarea name="content" value={values.content} onChange={handleChange} />
+          <button type="submit">전송</button>
+        </form> 
     );
     
 }
