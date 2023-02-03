04bd8222e88f8c4db83acf7a27e80c4a847b6681
JoSooha <dlrhdcjs@naver.com>
2023-02-03 23:56:54 +0900

    eg 파일인풋 수정

--- movie-app/src/components/FileInput.js
+++ movie-app/src/components/FileInput.js
@@ -4,10 +4,11 @@ function FileInput () {
     const [file, setFile] = useState()
     
     const handleFile = (e) => {
-        console.log(e.target.files)
+        const newFile = e.target.files[0]
+        setFile(newFile)
     }
 
-    return (<input type="file" value={file} onChange={handleFile}></input>) //input 태그 하나 주는 것
+    return (<input type="file" onChange={handleFile}></input>) //input 태그 하나 주는 것
 } //type=file 만 적어도 업로드 버튼 형식 갖춰짐 / onChange로 파일 올린 뒤 컨트롤
 
 export default FileInput
