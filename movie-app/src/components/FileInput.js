df6d4576592c817cad585fdf47479b3621dcf038
JoSooha <dlrhdcjs@naver.com>
2023-02-04 01:31:16 +0900

    eg useRef

--- movie-app/src/components/FileInput.js
+++ movie-app/src/components/FileInput.js
@@ -1,11 +1,20 @@
+import { useEffect, useRef } from "react";
+
 function FileInput({ name, value, onChange }) { //value는 후에 미리보기 등으로 활용 가능해서 일단 받아놓음..
-  
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue); //부모의 것 실행 (prop으로 받음)
   };
 
-  return <input type="file" onChange={handleChange} />;
-} //input 값 바뀌면 위 함수 호출, 여기서 onChange는 위 받은 것과 별개
+  useEffect(()=>{
+    if(inputRef.current) {
+        console.log(inputRef)
+    }
+  }, [])
+
+  return <input type="file" onChange={handleChange} ref={inputRef}/>;
+} 
 
 export default FileInput;
